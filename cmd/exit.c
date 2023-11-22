5e4553935fe554c58ad5975115a31b38d668f3ee
LE BOULC'H erin <erin.le-boulch@etu.u-paris.fr>
2023-11-22 12:15:18 +0100

    Exit

--- cmd/exit.c
+++ cmd/exit.c
@@ -1,8 +1,16 @@
 #include "../lib/exit.h"
+#include "../lib/env.h"
+#include <stdio.h>
 
 void exit_jsh(){
-    //TODO 
+    if(jobs != 0){
+        printf("Des jobs sont en cours d'exécution ou suspendus.\n");
+        val_retour = 1;
+        return;
+    }
+    boucle = false;
 }
-void exit_jsh_with_arg(int i){
-    //TODO 
+void exit_jsh_with_arg(int val){
+    val_retour = val;
+    exit_jsh();
 }
